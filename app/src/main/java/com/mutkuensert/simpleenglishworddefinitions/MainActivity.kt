02b5de59823ef8167e5ce90f36a694e81d540b4e
Mustafa Utku Ensert <mutkuensert@hotmail.com>
2022-05-01 02:05:49 +0300

    Ui almost completed.

--- app/src/main/java/com/mutkuensert/simpleenglishworddefinitions/MainActivity.kt
+++ app/src/main/java/com/mutkuensert/simpleenglishworddefinitions/MainActivity.kt
@@ -3,11 +3,15 @@ package com.mutkuensert.simpleenglishworddefinitions
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mutkuensert.simpleenglishworddefinitions.ui.theme.SimpleEnglishWordDefinitionsTheme
@@ -18,26 +22,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             SimpleEnglishWordDefinitionsTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    SimpleEnglishWordDefinitionsTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/mutkuensert/simpleenglishworddefinitions/MainScreen.kt
@@ -0,0 +1,89 @@
+package com.mutkuensert.simpleenglishworddefinitions
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.unit.dp
+import com.mutkuensert.simpleenglishworddefinitions.ui.theme.DarkBlue
+import com.mutkuensert.simpleenglishworddefinitions.ui.theme.LightBlue
+
+@Composable
+fun MainScreen() {
+    Surface(modifier = Modifier.fillMaxSize(),
+    color = MaterialTheme.colors.primary) {
+        searchArea()
+    }
+}
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+fun searchArea(){
+    val (searchText, setText) = remember { mutableStateOf("") }
+    var contentVisibility by remember { mutableStateOf(false)}
+
+    var response by remember { mutableStateOf("Response")}
+
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Spacer(modifier = Modifier.height(35.dp))
+
+        Card(modifier = Modifier
+            .padding(12.dp)
+            .shadow(elevation = 10.dp)
+            .fillMaxWidth(),
+            backgroundColor = MaterialTheme.colors.primary) {
+            
+            Row(verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(vertical = 10.dp)) {
+                
+                CustomTextField(text = searchText, setValue = setText,
+                    modifier = Modifier
+                        .weight(5f)
+                        .padding(start = 15.dp, bottom = 5.dp))
+
+                Spacer(modifier = Modifier.width(20.dp))
+
+                Button(onClick = { contentVisibility = !contentVisibility },
+                    elevation = ButtonDefaults.elevation(defaultElevation = 10.dp),
+                    modifier = Modifier
+                        .weight(2f)
+                        .padding(end = 15.dp)) {
+                    Text(text = "Search")
+                }
+            }
+        }
+
+        AnimatedVisibility(visible = contentVisibility) {
+            Card(modifier = Modifier
+                .padding(12.dp)
+                .shadow(elevation = 10.dp)
+                .fillMaxWidth(),
+                backgroundColor = MaterialTheme.colors.primary) {
+                AnimatedContent(targetState = response, modifier = Modifier.padding(10.dp)) {
+                    Text(text = response)
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun CustomTextField(text: String, setValue: (String)-> Unit, modifier: Modifier = Modifier){
+    OutlinedTextField(modifier = modifier,
+        value = text,
+        onValueChange = setValue,
+        label = { Text("Search") },
+        singleLine = true,
+        colors = TextFieldDefaults.outlinedTextFieldColors(focusedBorderColor = LightBlue, focusedLabelColor = DarkBlue, unfocusedBorderColor = LightBlue))
+}
