edaa879583bbc07c7314f0e1213b320f76a86f5f
Mustafa Utku Ensert <mutkuensert@hotmail.com>
2022-05-01 02:08:39 +0300

    Paketler oluşturuldu

--- app/src/main/java/com/mutkuensert/simpleenglishworddefinitions/view/MainActivity.kt
+++ app/src/main/java/com/mutkuensert/simpleenglishworddefinitions/view/MainActivity.kt
@@ -1,19 +1,9 @@
-package com.mutkuensert.simpleenglishworddefinitions
+package com.mutkuensert.simpleenglishworddefinitions.view
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.mutkuensert.simpleenglishworddefinitions.MainScreen
 import com.mutkuensert.simpleenglishworddefinitions.ui.theme.SimpleEnglishWordDefinitionsTheme
 
 class MainActivity : ComponentActivity() {
