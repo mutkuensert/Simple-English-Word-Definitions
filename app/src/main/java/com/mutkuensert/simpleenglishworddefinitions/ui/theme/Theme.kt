4eaf9e62945e57025c3cddb1ef4706f27d51142b
Mustafa Utku Ensert <mutkuensert@hotmail.com>
2022-05-10 00:43:39 +0300

    Transparent status bar feature added.

--- app/src/main/java/com/mutkuensert/simpleenglishworddefinitions/ui/theme/Theme.kt
+++ app/src/main/java/com/mutkuensert/simpleenglishworddefinitions/ui/theme/Theme.kt
@@ -5,7 +5,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -41,6 +43,11 @@ fun SimpleEnglishWordDefinitionsTheme(
     } else {
         LightColorPalette
     }*/
+
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setSystemBarsColor(Color.Transparent,true)
+    }
 
     MaterialTheme(
         colors = LightColorPalette,
