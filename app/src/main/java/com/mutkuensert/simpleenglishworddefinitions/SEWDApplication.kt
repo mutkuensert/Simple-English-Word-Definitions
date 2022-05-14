75c95ad8c53b138c45bfcc4abdb30b6210d39c35
Mustafa Utku Ensert <mutkuensert@hotmail.com>
2022-05-14 18:35:16 +0300

    Hilt application class has been added.

---
+++ app/src/main/java/com/mutkuensert/simpleenglishworddefinitions/SEWDApplication.kt
@@ -0,0 +1,8 @@
+package com.mutkuensert.simpleenglishworddefinitions
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class SEWDApplication: Application() {
+}
